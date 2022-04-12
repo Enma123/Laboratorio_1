387f0d3b97bd5da46a25b31fc92d02e4486770dd
felioxd <83518718+felioxd@users.noreply.github.com>
2022-04-12 17:43:24 -0600

    Cambio nombre Crossover e implementación clase

---
+++ Laboratorio_1/src/carros/Crossover.java
@@ -0,0 +1,19 @@
+package carros;
+
+import motores.Motor;
+
+public class Crossover extends CarroBase{
+
+	public Crossover(Motor motor) {
+		super(motor);
+	}
+	
+	public void imprimirCategoria(){
+		System.out.println("Soy Crossover");
+		imprimirMotor();
+	}
+	
+	public void imprimirCategoria(String name) {
+		System.out.println(name);
+	}
+}
--- Laboratorio_1/src/carros/crossover.java
+++
@@ -1,5 +0,0 @@
-package carros;
-
-public class crossover {
-
-}
